5b184240449dde0aa3746b18b7c70d4eed19eb7a
vvt2001 <81843040+vvt2001@users.noreply.github.com>
2021-04-04 20:41:30 +0700

    Update thuoc.java

--- src/main/java/com/nst/thuoc.java
+++ src/main/java/com/nst/thuoc.java
@@ -1,7 +1,47 @@
 package com.nst;
 
 public class thuoc {
-    int b;
-    int c;
-    int v;
+
+  private String maThuoc;
+  private String tenThuoc;
+  private int soLuongTonKho;
+  private double giaNhap1DonVi;
+  private double heSoLai;
+  private double giaXuat = giaNhap1DonVi * heSoLai;
+
+  public void setCode(String code) {
+    this.maThuoc = code;
+  }
+  public void setName(String name) {
+    this.tenThuoc = code;
+  }
+  public void setStocks(int stocks) {
+    this.soLuongTonKho = stocks;
+  }
+  public void setPriceIn(double PriceIn) {
+    this.giaNhap1DonVi = PriceIn;
+  }
+  public void setCoeff(double coeff) {
+    this.heSoLai = coeff;
+  }
+
+  public String getCode(String maThuoc) {
+    return maThuoc;
+  }
+  public String getName(String tenThuoc) {
+    return tenThuoc;
+  }
+  public int getStocks(int soLuongTonKho) {
+    return soLuongTonKho;
+  }
+  public double getPriceIn(double giaNhap1DonVi) {
+    return giaNhap1DonVi;
+  }
+  public double getCoeff(double heSoLai) {
+    return heSoLai;
+  }
+  public double getPriceOut(double giaXuat) {
+    return giaXuat;
+  }
+
 }
