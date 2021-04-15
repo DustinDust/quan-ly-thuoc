bebbaad412e72b98529d416e515e7202052b4fef
vvt2001 <81843040+vvt2001@users.noreply.github.com>
2021-04-15 20:16:28 +0700

    Update LiquidMedicine.java

--- src/main/java/com/nst/Medicine/LiquidMedicine.java
+++ src/main/java/com/nst/Medicine/LiquidMedicine.java
@@ -3,17 +3,18 @@ package com.nst.Medicine;
 public class LiquidMedicine extends Medicine {
 
     private double ml;
-    public LiquidMedicine(String code, String name, double stocks, double priceIN, double coeff, String color, String shape) {
+    LiquidMedicine(String code, String name, int stocks, double priceIN, double coeff, String color, String shape, int numberOut,double ml) { 
       super(code, name, stocks, priceIN, coeff, color, shape);
-    }
+      this.ml=ml;
+    }  
     public void setMg(double ml) {
       this.ml = ml;
     }
-
+  
     public double getMg(double ml) {
       return ml;
     }
-
+    
     public double priceOUT(double priceIN, double coeff, double ml){
         return ml * priceIN * coeff;
     }
